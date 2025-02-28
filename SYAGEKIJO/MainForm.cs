ce3889312c8601591cf91e3467ffd0e2f999f74b
HirofumiIsaka <uni202412_02@telework.to>
2025-02-28 15:26:02 +0900

    テスト

--- SYAGEKIJO/MainForm.cs
+++ SYAGEKIJO/MainForm.cs
@@ -13,21 +13,19 @@ using NAudio.Wave;
 
 namespace SYAGEKIJO
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-
         //タイマー間隔
         private const int TimerInterval = 15;
-
-
 
         private WaveOutEvent outputDevice1;
 
         MediaPlayer player1;
-        private AudioFileReader audioFile1;
-        public Form1()
+        public MainForm()
         {
-            InitializeComponent();
+
+         
+        InitializeComponent();
             // audioFile1 = new AudioFileReader(@"Sounds\ks039.wav");
 
             // 出力デバイスの初期化
@@ -50,6 +48,32 @@ namespace SYAGEKIJO
             daruma.Left += 10;
             oni.Left += 10;
             syougi.Left += 10;
+            if (daruma.Left > 1000)
+            {
+                daruma.Left = 0;
+            }
+            if (oni.Left > 1000)
+            {
+                oni.Left = 0;
+            }
+            if(syougi.Left > 1000)
+            {
+                syougi.Left = 0;
+            }
+            if (star.Left > 1000)
+            {
+                star.Left = 0;
+            }
+            if (pictureBox1Player.Bounds.IntersectsWith(pictureBox1Falling.Bounds)
+            {
+                // 衝突時の処理（例：得点加算、メッセージ表示、落ち物の再配置など）
+                ResetFallingObject();
+                MessageBox.Show("射撃成功！");
+            }
+            else
+            {          
+                Console.WriteLine("ゲームオーバー");
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -68,11 +92,30 @@ namespace SYAGEKIJO
             outputDevice1.Stop();
 
         }
-        int remeinTime = 10;
+        int remeinTime = 120;
+        private object e;
+        private object pictureBox1Player;
 
         private void timer2_Tick(object sender, EventArgs e)
         {
             time.Text = (remeinTime--).ToString();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int moveSpeed = 20; // 移動量の調整
+
+            if (e.KeyCode == Keys.Up)
+            {
+                // 画面上端を越えないように移動
+                if (player.Top - moveSpeed >= 0)
+                    player.Top -= moveSpeed;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                // 画面下端を越えないように移動
+                if (player.+ moveSpeed <= this.ClientSize.Height)
+                    player.u+= moveSpeed;
+            }
         }
     }
 }
